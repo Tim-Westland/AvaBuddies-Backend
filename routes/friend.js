5f5e70228e0875bf3b453be006cbd761d503811e
yoerivanhoof <y.vanhoof@student.avans.nl>
2019-04-08 13:01:57 +0200

    added error messages

--- routes/friend.js
+++ routes/friend.js
@@ -5,9 +5,9 @@ const friend = require('../models/friends');
 /* GET home page. */
 router.get("/requests", function(req, res, next) {
     friend.find({friend1:req.user._id, confirmed: false}).exec(function (err, own_requests) {
-        if(err) return res.status(500).json({message:"an error occured"});
+        if(err) return res.status(500).json({message:"an error occured "+err});
        friend.find({friend2:req.user._id, confirmed: false}).exec(function (err, requests) {
-           if(err) return res.status(500).json({message:"an error occured"});
+           if(err) return res.status(500).json({message:"an error occured "+err});
            res.json({own_requests: own_requests, requests: requests});
        })
     });
@@ -15,22 +15,23 @@ router.get("/requests", function(req, res, next) {
 
 router.get("/allconnections", function(req, res, next) {
     friend.find({$or:[{friend1: req.user._id},{friend2:req.user._id}]}).exec(function (err, connections) {
-        if(err) return res.status(500).json({message:"an error occured"});
+        if(err) return res.status(500).json({message:"an error occured "+err});
         res.json({connections:connections});
     });
 });
 
 router.post("/dorequest", function(req,res,next){
+    if(!req.body.friend) return res.status(422).json({message:"missing friend id in body"});
     var friends = new friend({friend1:req.user._id, friend2: req.body.friend, confirmed: false});
     friends.save(function (err) {
-       if (err) return res.status(500).json({message:"error"});
+       if (err) return res.status(500).json({message:"error while saving "+err});
     });
     res.json({message: "success"});
 });
 
 router.post("/friends", function(req,res,next){
     friend.find({$or:[{friend1: req.user._id},{friend2:req.user._id}],confirmed: true}).exec( function (err, result) {
-        if(err) return res.status(500).json({message:error});
+        if(err) return res.status(500).json({message:err});
         res.json({friends: result});
     });
 

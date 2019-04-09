9f72319086ff186c709977038e99746661442e21
Simon Heij <sheij1@avans.nl>
2019-04-09 16:29:50 +0200

    Added friend request validate for QR code

--- models/friends.js
+++ models/friends.js
@@ -15,6 +15,11 @@ const FriendSchema = new Schema({
         type:Boolean,
         required: true,
         default: false
+    },
+    validated: {
+        type:Boolean,
+        required: true,
+        default: false
     }
 
 });

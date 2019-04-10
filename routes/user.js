d075c93000d339991b9551b1d55cc73a6554c734
Tim Westland <tr.westland@student.avans.nl>
2019-04-10 17:38:34 +0200

    user can update aboutme or location fix get user with id

--- routes/user.js
+++ routes/user.js
@@ -10,42 +10,61 @@ router.get('/profile', (req, res, next) => {
     _id: req.user._id
   }).exec(function(err, result) {
     res.json({
-      user : result,
+      user: result,
     })
   })
 });
 
-router.get("/user/:id", (req,res,next)=>{
-  User.findOne({_id:req.params.id}).exec(function (err, result) {
-    if(err) return res.status(500).json({message:"could not find user."});
+router.get("/user/:id", (req, res, next) => {
+  User.findOne({
+    _id: req.params.id
+  }).exec(function(err, result) {
+    if (err) return res.status(500).json({
+      message: "could not find user."
+    });
 
-    var info = user;
+    var info = result;
     delete info.password;
     delete info.isAdmin;
-    res.json({user:info});
+    res.json({
+      result: info
+    });
   })
 });
 
-router.post('/updateprofile', (req,res)=>{
-  User.updateOne({ _id: req.user._id },
-    { aboutme: req.body.aboutme,
-      sharelocation: req.body.sharelocation}).exec(function (err, result) {
-        res.json('success');
-      })
+router.post('/updateprofile', (req, res) => {
 
+  var fields = {}
+  if (req.body.aboutme) {
+    fields.aboutme = req.body.aboutme
+  }
+  if (req.body.sharelocation) {
+    fields.sharelocation = req.body.sharelocation
+  }
+
+  User.updateOne({
+      _id: req.user._id
+    },
+    fields).exec(function(err, result) {
+    res.json('success');
+  })
 });
 
-router.post('/updateprofilepicture', (req,res)=>{
-  User.updateOne({ _id: req.user._id }, { image: req.body.image})
-  .exec(function (err, result) {
+router.post('/updateprofilepicture', (req, res) => {
+  User.updateOne({
+      _id: req.user._id
+    }, {
+      image: req.body.image
+    })
+    .exec(function(err, result) {
       res.json('success');
     })
 });
 
-router.get('/list', (req,res) => {
-  User.find().exec(function (err, result) {
+router.get('/list', (req, res) => {
+  User.find().exec(function(err, result) {
     res.json({
-        users : result
+      users: result
     });
   })
 });
@@ -55,12 +74,19 @@ router.delete('/destroy/:id', (req, res) => {
   User.deleteOne({
     _id: id
   }).exec();
-  res.json({status: 'success'})
+  res.json({
+    status: 'success'
+  })
 });
 
 router.get('/find/:keyword', (req, res) => {
   var keyword = req.params.keyword;
-  User.find( { 'name' : { '$regex' : keyword, '$options' : 'i' } } ).exec(function (err, result) {
+  User.find({
+    'name': {
+      '$regex': keyword,
+      '$options': 'i'
+    }
+  }).exec(function(err, result) {
     res.json(result);
   })
 });
@@ -72,14 +98,15 @@ router.get('/switchrole', (req, res, next) => {
   }).
   // select('_id title').
   exec(function(err, user) {
-    if(user.isAdmin == false){
+    if (user.isAdmin == false) {
       user.isAdmin = true
-    }
-    else{
+    } else {
       user.isAdmin = false
     }
     user.save();
-    res.json({status: 'success'})
+    res.json({
+      status: 'success'
+    })
   })
 });
 

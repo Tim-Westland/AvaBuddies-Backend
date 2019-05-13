aef83a1c3c58f0ed28342a198ea400cf0926618e
Simon Heij <dykeviewstudios@gmail.com>
2019-05-13 16:30:05 +0200

    Chat receive

--- controllers/chatMessageController.js
+++ controllers/chatMessageController.js
@@ -1,7 +1,20 @@
 const ChatMessage = require('../models/chatMessage');
+const Chat = require('../models/chat');
 
-exports.connect = function (socket) {
-    console.log('a user connected new '+socket.id);
-
-
+exports.listen = function (socket) {
+    socket.on('user online', userOnline);
+    Chat.find({}).exec(function (err, chats) {
+        chats.forEach(function (item) {
+            socket.on(item._id, messageReceved);
+        });
+    });
 };
+
+function userOnline(userId) {
+ console.log('user online with userId: ' + userId);
+}
+
+function messageReceved(message) {
+    console.log('Message receved: ' + message);
+
+}
--- controllers/socketController.js
+++ controllers/socketController.js
@@ -2,9 +2,10 @@ const chatMessageController = require('./chatMessageController');
 
 exports.connect = function (socket) {
     console.log('Socket connected to socketController '+socket.id);
-    socket.on('disconnect', disconnect)
+    socket.on('disconnect', disconnect);
 
-    
+    chatMessageController.listen(socket);
+
 };
 
 function disconnect() {
